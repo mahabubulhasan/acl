977f287ce2a93e46b355d00637e77b35a7dcfa26
Mahabubul Hasan <codehasan@gmail.com>
2018-02-15 16:05:39 +0600

    unit test

---
+++ tests/RoleServiceTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @author: Mahabubul Hasan <codehasan@gmail.com>
+ * Date: 2/15/2018
+ * Time: 2:57 PM
+ */
+namespace Uzzal\Acl\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Uzzal\Acl\Services\RoleService;
+
+class RoleServiceTest extends TestCase
+{
+    public function testGetNewAndDeletedPermissions(){
+        $service = new RoleService();
+        $old = ['A','B', 'C', 'D'];
+        $new = ['C', 'D', 'E', 'F'];
+
+        $output = [
+            'insert'=>[2=>'E', 3=>'F'],
+            'delete'=>[0=>'A', 1=>'B']
+        ];
+
+        $this->assertEquals($output, $service->getNewAndDeletedPermissions($old, $new));
+    }
+}
