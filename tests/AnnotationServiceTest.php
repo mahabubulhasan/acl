1b053691b3848ddb10e3d4d8a41e9fa9902dd9e3
Mahabubul Hasan <codehasan@gmail.com>
2018-03-11 20:51:45 +0600

    unit test added for annotation

---
+++ tests/AnnotationServiceTest.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mahabubul Hasan
+ * Date: 3/11/2018
+ * Time: 8:12 PM
+ */
+
+namespace Uzzal\Acl\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Uzzal\Acl\Services\AnnotationService;
+
+class AnnotationServiceTest extends TestCase
+{
+    public function testAnnotations(){
+        $obj = new AnnotationService('Uzzal\Acl\Tests\AnnotationDummy@dummyMethod');
+        $this->assertEquals('Default, Admin', $obj->getAllowRole());
+        $this->assertEquals('This is a dummy resource', $obj->getResource());
+    }
+}
+
+class AnnotationDummy{
+    /**
+     * @resource('This is a dummy resource')
+     * @allowRole('Default, Admin')
+     */
+    public function dummyMethod(){
+       return true;
+    }
+}
