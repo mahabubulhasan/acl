c6acb9e3db34622d1003ac7a7395d55a3b918001
Mahabubul Hasan <codehasan@gmail.com>
2018-03-15 13:45:51 +0600

    layout updated for bootstrap 4

--- src/views/role/create.blade.php
+++ src/views/role/create.blade.php
@@ -2,61 +2,62 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
+    <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
-            <li><a href="/">Home</a></li>
-            <li><a href="/role">Role</a></li>
-            <li class="active">Create New</li>
+            <li class="breadcrumb-item"><a href="/">Home</a></li>
+            <li class="breadcrumb-item"><a href="/role">Role</a></li>
+            <li class="breadcrumb-item active">Create Role</li>
         </ol>
-        <div class="col-md-8 col-md-offset-2">            
-            <div class="panel panel-default">
-                <div class="panel-heading">New Role</div>
-                <div class="panel-body">
-                    @if (count($errors) > 0)
-                    <div class="alert alert-danger">
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
+    </nav>
+
+    @if (count($errors) > 0)
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <div class="card">
+        <div class="card-header">
+            <h4>Create Role</h4>
+        </div>
+        <div class="card-body">
+            <form role="form" method="POST" action="{{ url('/role/store') }}">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+                <div class="form-group row">
+                    <label class="col-sm-3 control-label">Name</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" name="name" value="{{ old('name') }}" placeholder="Role name...">
                     </div>
-                    @endif
+                </div>
 
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/role/store') }}">
-                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-
-                        <div class="form-group">
-                            <label class="col-md-4 control-label">Name</label>
-                            <div class="col-md-6">
-                                <input type="text" class="form-control" name="name" value="{{ old('name') }}">
-                            </div>
-                        </div>
-
-                        @foreach($resources as $k=>$actions)
-                        <div class="form-group">
-                            <label class="col-md-4 control-label">{{ $k }}</label>
-                            <div class="col-md-6">
-                                @foreach($actions as $act)
+                @foreach($resources as $k=>$actions)
+                    <div class="form-group row">
+                        <label class="col-sm-3 control-label">{{ $k }}</label>
+                        <div class="col-sm-9">
+                            @foreach($actions as $act)
                                 <div class="checkbox">
                                     <label>
                                         <input name="resource[]" value="{{ $act['id'] }}" type="checkbox"> {{ $act['name'] }}
                                     </label>
                                 </div>
-                                @endforeach
-                            </div>
+                            @endforeach
                         </div>
-                        @endforeach
+                    </div>
+                @endforeach
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Create
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                <div class="form-group row">
+                    <div class="offset-sm-3 col-sm-9">
+                        <button type="submit" class="btn btn-sm btn-primary">
+                            Create
+                        </button>
+                    </div>
                 </div>
-            </div>
+            </form>
         </div>
     </div>
 </div>
--- src/views/role/edit.blade.php
+++ src/views/role/edit.blade.php
@@ -2,62 +2,63 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
+    <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
-            <li><a href="/">Home</a></li>
-            <li><a href="/role">Role</a></li>
-            <li class="active">Edit</li>
+            <li class="breadcrumb-item"><a href="/">Home</a></li>
+            <li class="breadcrumb-item"><a href="/role">Role</a></li>
+            <li class="breadcrumb-item active">Edit Role</li>
         </ol>
-        <div class="col-md-8 col-md-offset-2">            
-            <div class="panel panel-default">
-                <div class="panel-heading">Edit Role</div>
-                <div class="panel-body">
-                    @if (count($errors) > 0)
-                    <div class="alert alert-danger">
-                        <strong>Whoops!</strong> There were some problems with your input.<br><br>
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
+    </nav>
+
+    @if (count($errors) > 0)
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+
+    <div class="card">
+        <div class="card-header">
+            <h4>Edit Role</h4>
+        </div>
+        <div class="card-body">
+            <form class="form-horizontal" role="form" method="POST" action="{{ url('/role/update', ['id' => $id]) }}">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+                <div class="form-group row">
+                    <label class="col-sm-3 control-label">Name</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" placeholder="Role name..." name="name" value="{{ ($old = old('name'))?$old:$role->name }}">
                     </div>
-                    @endif
+                </div>
 
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/role/update', ['id' => $id]) }}">
-                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-
-                        <div class="form-group">
-                            <label class="col-md-4 control-label">Name</label>
-                            <div class="col-md-6">
-                                <input type="text" class="form-control" name="name" value="{{ ($old = old('name'))?$old:$role->name }}">
-                            </div>
-                        </div>
-                        
-                        @foreach($resources as $k=>$actions)
-                        <div class="form-group">
-                            <label class="col-md-4 control-label">{{ $k }}</label>
-                            <div class="col-md-6">
-                                @foreach($actions as $act)
+                @foreach($resources as $k=>$actions)
+                    <div class="form-group row">
+                        <label class="col-sm-3 control-label">{{ $k }}</label>
+                        <div class="col-sm-9">
+                            @foreach($actions as $act)
                                 <div class="checkbox">
                                     <label>
                                         <input name="resource[]" value="{{ $act['id'] }}" type="checkbox" {{ (in_array($act['id'], $permissions))?'checked="checked"':'' }}> {{ $act['name'] }}
                                     </label>
                                 </div>
-                                @endforeach
-                            </div>
+                            @endforeach
                         </div>
-                        @endforeach
-                        
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Update
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    </div>
+                @endforeach
+
+                <div class="form-group row">
+                    <div class="col-sm-9 offset-sm-3">
+                        <button type="submit" class="btn btn-sm btn-primary">
+                            Update
+                        </button>
+                    </div>
                 </div>
-            </div>
+            </form>
         </div>
     </div>
 </div>
